a03d977a86481174f735de7b4940ba2ecc15f4b4
ChoiJaeHoon <zpqls321@naver.com>
2020-10-21 23:59:47 +0900

    Emlpoyee controller

--- src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
+++ src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class EmployeeController {
     
+    @GetMapping("/Employee")
+    public String getEmployees(){
+        return "Employee";
+    }
 }
