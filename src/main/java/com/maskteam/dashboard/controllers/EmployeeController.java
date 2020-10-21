3d1aa2b5ae4d4eda5b07e3ef10c123ca07bb84d4
ChoiJaeHoon <zpqls321@naver.com>
2020-10-22 00:02:28 +0900

    import getmapping

--- src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
+++ src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.maskteam.dashboard.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
