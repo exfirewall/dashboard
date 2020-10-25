3f246f220cf085fbac9ffedc916c7c26d93b4551
ChoiJaeHoon <zpqls321@naver.com>
2020-10-25 23:46:09 +0900

    Add Employee services

--- src/main/java/com/maskteam/dashboard/services/EmployeeService.java
+++ src/main/java/com/maskteam/dashboard/services/EmployeeService.java
@@ -1,8 +1,19 @@
 package com.maskteam.dashboard.services;
 
+import java.util.List;
+
+import com.maskteam.dashboard.models.Employee;
+import com.maskteam.dashboard.repositories.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
 }
