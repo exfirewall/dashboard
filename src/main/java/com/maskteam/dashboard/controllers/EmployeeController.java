9ba781a5f3650d5cff74a4f1e2fa8d4657c8849d
ChoiJaeHoon <zpqls321@naver.com>
2020-10-25 23:59:12 +0900

    Fix employeeController.java

--- src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
+++ src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.maskteam.dashboard.controllers;
 
+import java.util.List;
+import com.maskteam.dashboard.services.EmployeeService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
     
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/Employee")
-    public String getEmployees(){
+    public String getEmployees(Model model){
+
+        model.addAllAttributes("employees", employeeList);
         return "Employee";
     }
 }
