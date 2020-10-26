bfb28de192ad29b011e43a90ca83c483a843d21e
ChoiJaeHoon <zpqls321@naver.com>
2020-10-26 23:43:23 +0900

    Sync error

--- src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
+++ src/main/java/com/maskteam/dashboard/controllers/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.maskteam.dashboard.controllers;
 
 import java.util.List;
+
+import com.maskteam.dashboard.models.Employee;
 import com.maskteam.dashboard.services.EmployeeService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +19,10 @@ public class EmployeeController {
     @GetMapping("/Employee")
     public String getEmployees(Model model){
 
-        model.addAllAttributes("employees", employeeList);
+        List<Employee> employeeList = employeeService.getEmployees();
+
+        model.addAttribute("employee", employeeList);
+
         return "Employee";
     }
 }
