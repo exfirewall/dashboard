7a2503628df4a4beccfd8aa637020b814e0d93d1
ChoiJaeHoon <zpqls321@naver.com>
2020-10-30 02:00:40 +0900

    Add client constructor to send data

--- src/main/java/com/maskteam/dashboard/AppController.java
+++ src/main/java/com/maskteam/dashboard/AppController.java
@@ -3,11 +3,16 @@ package com.maskteam.dashboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.maskteam.dashboard.client.RaspClient;
 @Controller
 public class AppController {
     
     @GetMapping("/index")
     public String goHome(){
+
+        RaspClient client = new RaspClient(1);
+        System.out.println(client.getResult());
+
         return "index";
     }
 }
