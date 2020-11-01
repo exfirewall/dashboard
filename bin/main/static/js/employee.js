c3d717f84a8eea422edb20a598ba53f112d6d695
ChoiJaeHoon <zpqls321@naver.com>
2020-11-01 22:55:40 +0900

    Add update fuction

--- bin/main/static/js/employee.js
+++ bin/main/static/js/employee.js
@@ -1,5 +1,14 @@
 $('document').ready(function(){
     $('table #editButton').on('click', function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $.get(href, function(country, status){
+            $('#idEdit').val(country.id);
+            $('#empnoEdit').val(country.empno);
+            $('#enameEdit').val(country.ename);
+            $('#jobEdit').val(country.job);
+            $('#deptIdEdit').val(country.deptno);
+        });
         $('#editModal').modal();
     });
 });
